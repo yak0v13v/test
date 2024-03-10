1431738a4e0601b4c71ab77d1ad0f3391feef727
Artem Iakovlev <iakovlev.artem.a@gmail.com>
2024-03-10 19:05:52 +0400

    add path to remote images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://d2norla3tyc4cn.cloudfront.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
